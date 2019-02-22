b7c69f1acac25cca5e18bbd01bd70ea97bd2507a
ProtoZxn99 <patrick.soebiantoro@gmail.com>
2019-02-22 22:17:46 +0700

    Added email check

--- Server/WiRe/resetAccountPassword.php
+++ Server/WiRe/resetAccountPassword.php
@@ -11,11 +11,19 @@ include 'modules/FormatUtils.php';
 
 $account_email = mysqli_real_escape_string($conn, $_POST['account_email']);
 
-$msg = "<p align='center'>Please click this link to reset your password at WiRe: </p><br>";
-$url = "http://".$ip_wire."/WiRe/FormAccountPasswordChange.php?account_email=".hash("sha256",$header_salt.$account_user.$end_salt);
-$img = "<img src = 'https://i.ytimg.com/vi/V015SjjbYXE/maxresdefault.jpg'>";
-$closure = "If you didn't remember applying for a password reset, you can leave this email alone.";
-sendeMail("wire@noreply.com", $account_user, "Reset WiRe Password", $msg."<a href ='".$url."'>".$img."</a>".$closure);
-echo 1;
+$query = mysqli_query($conn, "select count(*) as users from account where account_email = '".$account_email."';");
+$exec = mysqli_fetch_array($query);
+
+if($exec['users']>0){
+    $msg = "<p align='center'>Please click this link to reset your password at WiRe: </p><br>";
+    $url = "http://".$ip_wire."/WiRe/FormAccountPasswordChange.php?account_email=".hash("sha256",$header_salt.$account_user.$end_salt);
+    $img = "<img src = 'https://i.ytimg.com/vi/V015SjjbYXE/maxresdefault.jpg'>";
+    $closure = "If you didn't remember applying for a password reset, you can leave this email alone.";
+    sendeMail("wire@noreply.com", $account_user, "Reset WiRe Password", $msg."<a href ='".$url."'>".$img."</a>".$closure);
+    echo 1;
+}
+else{
+    echo $error['email_fail'];
+}
 
 include '_footer.php';
