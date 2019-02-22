95fa8e80282536eb63460af429217b777df1270d
ProtoZxn99 <patrick.soebiantoro@gmail.com>
2019-02-22 22:55:14 +0700

    Fixed missing parameters

--- Server/WiRe/setDeviceState.php
+++ Server/WiRe/setDeviceState.php
@@ -4,9 +4,10 @@ include '_header.php';
 $account_id = mysqli_real_escape_string($conn, $_POST['account_id']);
 $account_password = mysqli_real_escape_string($conn, $_POST['account_password']);
 $device_id = mysqli_real_escape_string($conn, $_POST['device_id']);
+$device_state = mysqli_real_escape_string($conn, $_POST['device_state']);
 
 ValidateUser($account_id, $account_password);
 
-$query = mysqli_query($conn, "update device set device_state = 1 where device_id = '".$device_id."';");
+$query = mysqli_query($conn, "update device set device_state = ".$device_state." where device_id = '".$device_id."';");
 
 include '_footer.php';
