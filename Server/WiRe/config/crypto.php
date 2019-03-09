61777aa1c2104118d3f4e1f06338a006aea6d9e9
ProtoZxn99 <patrick.soebiantoro@gmail.com>
2019-03-09 12:23:28 +0700

    Update crypto.php

--- Server/WiRe/config/crypto.php
+++ Server/WiRe/config/crypto.php
@@ -8,6 +8,6 @@
 
 $header_salt = "FRESHLY SMOKED Kosher salted ";
 $more_salt = "*SALT*";
-$end_salt = " sprinkled with more Sodium Chloride and a little of the best Indonesian tiny black peppers to suit your taste and protect you from evildoers";
+$end_salt = " sprinkled with more Sodium chloride and a little of the best Indonesian tiny black peppers to suit your taste and protect you from evildoers";
 
 $xor_mac_key = "ConcealM4CtoHackers";
