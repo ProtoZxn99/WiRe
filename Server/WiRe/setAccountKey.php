5514c49b7f0f1edbb7d4cb9c3cae80de98fab345
ProtoZxn99 <patrick.soebiantoro@gmail.com>
2019-04-13 18:14:39 +0700

    Added security features

--- Server/WiRe/setAccountKey.php
+++ Server/WiRe/setAccountKey.php
@@ -4,6 +4,21 @@ include '_header.php';
 $account_id = mysqli_real_escape_string($conn, $_POST['account_id']);
 $account_password = mysqli_real_escape_string($conn, $_POST['account_password']);
 $account_key = mysqli_real_escape_string($conn, $_POST['account_key']);
+$iv = mysqli_real_escape_string($conn, $_POST['iv']);
+$dummy_id = mysqli_real_escape_string($conn, $_POST['dummy_id']);
+
+ValidateUser($conn, $account_id, $account_password);
+
+$base_key = str_pad($account_id,16,"#",STR_PAD_LEFT);
+
+$cbc = new AES_128_CBC($base_key, $iv);
+
+$dummy_id = $cbc->decrypt($dummy_id);
+
+if(dummy_id!=account_id){
+	echo $GLOBALS['error']["id_fail"];
+	include '_footer.php';
+}
 
 echo DiffieHellman_Count($account_key, $account_id);
 
@@ -19,9 +34,8 @@ function DiffieHellman_Count($pub_client, $account_id){
 }
 
 function Save_Shared($key, $account_id){
-        $ecb = new AES_128_ECB($GLOBALS['crypto']['server_aes']);
-        $key = substr(base64_encode($key), 0, 16);
-        $ekey = $ecb->encrypt($key);
+	$ecb = new AES_128_ECB($GLOBALS['crypto']['server_aes']);
+	$key = substr(base64_encode($key), 0, 16);
 	$query = mysqli_query($conn, "update account set account_key = '".$key."' where account_id = ".$account_id.";");
 	$exec = mysqli_fetch_array($query);
 }
