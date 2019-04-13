ff47ea43c92a36704199beb5786c64f4be93d4a3
ProtoZxn99 <patrick.soebiantoro@gmail.com>
2019-04-13 14:27:06 +0700

    Fixed type
    Pass is changed to password

--- Server/WiRe/confirmAccountRegistration.php
+++ Server/WiRe/confirmAccountRegistration.php
@@ -3,14 +3,14 @@ include '_header.php';
 
 $confirmation_email = mysqli_real_escape_string($conn, $_GET['confirmation_email']);
 
-$query = mysqli_query($conn, "SELECT confirmation_email as email, account_pass as pass FROM confirmation;");
+$query = mysqli_query($conn, "SELECT confirmation_email as email, account_password as pass FROM confirmation;");
 
 $ecb = new AES_128_ECB($GLOBALS['crypto']['server_aes']);
 
 while($exec = mysqli_fetch_array($query)){
     $real_email = $ecb->decrypt($exec['email']);
     if(hash("sha256",$GLOBALS['crypto']['header_salt'].$real_email.$GLOBALS['crypto']['end_salt'])==$confirmation_email){
-        $moving = mysqli_query($conn, "insert into account (account_email, account_pass, account_block, account_key, account_time) values ('".$exec["email"]."','".$exec["pass"]."', 0, '',now());");
+        $moving = mysqli_query($conn, "insert into account (account_email, account_password, account_block, account_key, account_time) values ('".$exec["email"]."','".$exec["pass"]."', 0, '',now());");
         $delete = mysqli_query($conn, "delete from confirmation where confirmation_email = '".$exec["email"]."';");
         
         echo 1;
