6f081dcd293ee9f5ece67832e7de44961db20547
ProtoZxn99 <patrick.soebiantoro@gmail.com>
2019-04-26 22:55:02 +0700

    Replace

---
+++ getDeviceWiFiPassword.php
@@ -0,0 +1,30 @@
+<?php
+include '_header.php';
+
+$device_id = mysqli_real_escape_string($conn, XOR_Encrypt(base64_decode($_GET['device_id']),$GLOBALS['crypto']['xor_mac_key']));
+
+$hmac_id = substr($device_id, 0, 32);
+$device_id = substr($device_id, 32);
+
+if(MD5_HMAC($device_id, $GLOBALS['crypto']['xor_mac_key'], $GLOBALS['crypto']['xor_mac_key'])!=$hmac_id){
+    echo -1;
+    include '_footer.php';
+}
+
+$query = mysqli_query($conn, "select a.account_wifi_ssid as ssid, a.account_wifi_password as password from account a, device d where a.account_id = d.account_id and d.device_id = '".$device_id."-5' limit 1;");
+$exec = mysqli_fetch_array($query);
+
+if(isset($exec['password'])){
+	$ecb = new AES_128_ECB($GLOBALS['crypto']['server_aes']);
+
+	$ssid = $ecb->decrypt($exec['ssid']);
+	$password = $ecb->decrypt($exec['password']);
+	$epassword = base64_encode(XOR_Encrypt(MD5_HMAC($password, $GLOBALS['crypto']['xor_mac_key'], $ssid).$password, $ssid));
+
+	echo $epassword;
+}
+else{
+	echo -1;
+}
+
+include '_footer.php';
