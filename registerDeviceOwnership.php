b4bbe4f6ffebf0a32d6c60217b8787145061902a
ProtoZxn99 <patrick.soebiantoro@gmail.com>
2019-05-02 20:07:03 +0700

    Replace

--- registerDeviceOwnership.php
+++ registerDeviceOwnership.php
@@ -4,6 +4,7 @@ include '_header.php';
 $account_id = mysqli_real_escape_string($conn, $_POST['account_id']);
 $account_password = mysqli_real_escape_string($conn, $_POST['account_password']);
 $device_id = mysqli_real_escape_string($conn, $_POST['device_id']);
+$device_name = mysqli_real_escape_string($conn, $_POST['device_name']);
 $iv = mysqli_real_escape_string($conn, $_POST['iv']);
 
 $chat_key = ValidateUser($conn, $account_id, $account_password);
@@ -11,13 +12,14 @@ $chat_key = ValidateUser($conn, $account_id, $account_password);
 $cbc = new AES_128_CBC($chat_key, $iv);
 
 $device_id = $cbc->decrypt($device_id);
+$device_name = $cbc->decrypt($device_name);
 
 $ecb = new AES_128_ECB($GLOBALS['crypto']['device_aes']);
 
-$device_id = $ecb->encrypt($device_id);
+$device_id = $ecb->decrypt($device_id);
 
-$query = mysqli_query($conn, "update into device set account_id = ".$account_id." where device_id = '".$device_id."-%';");
+$query = mysqli_query($conn, "update device set account_id = '".$account_id."', device_name = '".$device_name."' where device_id = '".$device_id."';");
 
-echo 1;
+echo $query;
 
 include '_footer.php';
